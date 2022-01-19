f85375c7cba4f4cb97776c13c6588aafa40f671d
Sashankr <sashankr97@gmail.com>
2022-01-19 11:04:44 +0530

    Created signup page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import Landing from './pages/Landing';
 
 import {
   BrowserRouter as Router,
-  Switch,
+  Routes,
   Route,
 } from "react-router-dom";
+import SignupPage from './pages/SignupPage';
 
 function App() {
   return (
     <Router>
-      <Landing/>
+      <Routes>
+
+        <Route path="/" element={<Landing/>} />
+        <Route path="/signup" element={<SignupPage />} />
+
+      </Routes>
     </Router>
   );
 }
---
+++ src/components/Signup.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SignUpBackground from '../assets/images/signup-background.svg'
+import '../styles/Signup.css'
+
+
+const Signup = () => {
+    return (
+        <div className='signup' style={{backgroundImage : `url(${SignUpBackground})`}}>
+            
+        </div>
+    )
+}
+
+export default Signup
